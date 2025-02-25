a7be488a5d85076ab824fe75f5ba66cdc764b766
Daniel Amaya Montes <152107267+MONTESDANIEL@users.noreply.github.com>
2025-02-24 23:20:38 -0500

    feat: Configuración del enrutador.
    - Se crean los archivos base para vincular con las rutas.
    - Se configura el enrutador.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -8,10 +9,21 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from './components/NavBar.tsx';
 import Footer from './components/Footer.tsx';
 
+import Home from './pages/Home.tsx'
+import Books from './pages/Books.tsx';
+import Statistics from './pages/Statistics.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/statistics" element={<Statistics />} />
+      </Routes>
+    </Router>
     <Footer />
   </StrictMode>,
 )
---
+++ src/pages/Statistics.tsx
@@ -0,0 +1,11 @@
+const Statistics = () => {
+    return (
+        <section className="container-fluid">
+            <div className="row bg-body-tertiary p-3 m-3 rounded-4 min-vh-100">
+                <h1 className="text-center fw-bold p-3"> Estadísticas</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Statistics;
